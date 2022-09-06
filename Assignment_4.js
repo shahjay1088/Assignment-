ad74f351a7c95c273054a4cc1865202ef39c051f
Jay Shah <51516870+shahjay1088@users.noreply.github.com>
2022-09-06 08:12:30 +0530

    Add files via upload

---
+++ Assignment_4.js
@@ -0,0 +1,13 @@
+const matchHouses = (n)=>{
+    if(n===0 || n < 0 || typeof n !== "number")
+    {
+        return("Please Enter A Vailed Number")
+    }
+    else
+    {
+        let result;
+        result =(5*n)-1;
+        return result
+    }
+}
+console.log(matchHouses(4))
